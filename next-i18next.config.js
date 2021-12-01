73278c0f131189bd1d1c3014588c321d5d1b49cd
Stefan Natter <stefan@natter.at>
2021-12-01 21:19:20 +0100

    chore: fixed vercel deployment issue

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,3 +1,13 @@
+const path = require('path')
+
+/**
+ * HACK: make sure Vercel includes those files
+ * @see https://github.com/natterstefan/nextjs-template/pull/44#issuecomment-977663968
+ */
+if (typeof window === 'undefined') {
+  const _files = path.resolve('./public/locales')
+}
+
 module.exports = {
   i18n: {
     defaultLocale: 'en',
